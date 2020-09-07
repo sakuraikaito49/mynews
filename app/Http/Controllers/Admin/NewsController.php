959da1e7b511f309654aff837722b99c2b93add2
”名前” <hayato>
2020-09-07 05:54:54 +0000

    laravel 09　課題提出

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -14,3 +14,6 @@ class NewsController extends Controller
   }
   
 }
+
+
+  
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+Route::get('admin/news/create', 'Admin\AAAController@bbb');
+
+Route::get('admin/profile/create', 'Admin\ProfileController@add');
+
+Route::get('admin/profile/edit', 'Admin\profileController@edit');
